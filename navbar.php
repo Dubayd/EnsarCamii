e01cb8de6a904e1506127f9c3ae555546604ef5d
EhsanWA <169644@student.horizoncollege.nl>
2023-11-09 15:02:04 +0100

    navbar toegevoegd

---
+++ navbar.php
@@ -0,0 +1,35 @@
+     <!-- navbar -->
+     <main>
+       <nav class="navbar" aria-label="First navbar example">
+         <div class="container-fluid">
+           <a>
+             <h1>Ensar Camii</h1>
+           </a>
+           <!-- dropdown button -->
+           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample01" aria-controls="navbarsExample01" aria-expanded="false" aria-label="Toggle navigation">
+             <span class="navbar-toggler-icon"></span>
+           </button>
+
+           <div class="collapse navbar-collapse" id="navbarsExample01">
+             <ul class="navbar-nav me-auto mb-2">
+               <li class="nav-item">
+                 <a class="nav-link active" aria-current="page" href="index.html">Home</a>
+               </li>
+               <li class="nav-item">
+                 <a class="nav-link" href="contact.html" tabindex="-1" aria-disabled="false">contact</a>
+               </li>
+               <!-- dropdown -->
+               <li class="nav-item dropdown">
+                 <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-bs-toggle="dropdown" aria-expanded="false">Beleid & financiën</a>
+                 <ul class="dropdown-menu" aria-labelledby="dropdown01">
+                   <li><a class="dropdown-item" href="sp1.html">beleidsplan</a></li>
+                   <li><a class="dropdown-item" href="sp2.html">Financiën</a></li>
+                 </ul>
+               </li>
+             </ul>
+           </div>
+         </div>
+       </nav>
+     </main>
+     <!-- einde navbar -->
+    
